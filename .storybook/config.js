f445529a088dad96f27d08ceae4b1e6966357a45
Stephen Chiang <stephen.e.chiang@gmail.com>
2019-10-16 23:59:03 +0200

    Changed SB to dark

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,15 @@
+import { addParameters } from '@storybook/react';
+import { themes } from '@storybook/theming';
 import { configure, addDecorator } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import '../src/index.css';
+
+// Option defaults.
+addParameters({
+	options: {
+		theme: themes.dark
+	}
+});
 
 addDecorator(withA11y);
 // automatically import all files ending in *.stories.js
